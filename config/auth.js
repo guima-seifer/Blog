ed1a221b319b38af8119fe45f7cb819e7bba36b9
Helder Veloso <a70017@alunos.uminho.pt>
2018-01-11 13:32:42 +0000

    Adicionado o login com Google

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    'googleAuth' : {
+        'clientID'      : '778400540738-eapftb54vkr9gof3ijma2du7nao0p33b.apps.googleusercontent.com',
+        'clientSecret'  : 'yL1phj3A3ynoXkGxPpXQ27nK',
+        'callbackURL'   : 'http://localhost:3334/auth/google/callback'
+    }
+
+};
