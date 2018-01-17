3977255f03faa12161c6b5b0cc7fcb5f0516c87c
guima-seifer <fernando.b.v.guimaraes@gmail.com>
2018-01-17 23:22:04 +0000

    floating button, color remap, tooltips, notification button

--- public/js/scrolltop.js
+++ public/js/scrolltop.js
@@ -1,20 +1,21 @@
-$(document).ready(function () {
-  $(function () {
+$(document).ready(function() {
+  $(function() {
+    $('[data-toggle="tooltip"]').tooltip(),
 
-    $(document).on('scroll', function() {
+      $(document).on('scroll', function() {
 
-      if ($(window).scrollTop() > 100) {
-        $('.scroll-top-wrapper').addClass('show');
-      } else {
-        $('.scroll-top-wrapper').removeClass('show');
-      }
-    });
+        if ($(window).scrollTop() > 100) {
+          $('.scroll-top-wrapper').addClass('show');
+        } else {
+          $('.scroll-top-wrapper').removeClass('show');
+        }
+      });
 
     $('.scroll-top-wrapper').on('click', scrollToTop);
   });
 
   function scrollToTop() {
-    verticalOffset = typeof (verticalOffset) != 'undefined' ? verticalOffset : 0;
+    verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
     element = $('body');
     offset = element.offset();
     offsetTop = offset.top;
@@ -23,4 +24,37 @@ $(document).ready(function () {
     }, 500, 'linear');
   }
 
+  // TODO: terminar de configurar o delay nos tooltips dos floating buttons
+  // $(function () {
+  //   $btns = $('.btn-lg').find('button');
+  //   $btns.tooltip({
+  //     trigger: 'hover',
+  //     delay: {
+  //       show: 50,
+  //       hide: 50,
+  //     },
+  //     container: 'body',
+  //   });
+  //   $('.btn-lg').on('mouseenter', function (e) {
+  //     setTimeout(function () {
+  //       console.log('100');
+  //       setTooltips({
+  //         show: 100,
+  //         hide: 100,
+  //       });
+  //     }, 500);
+  //   }).on('mouseleave', function (e) {
+  //     console.log('500');
+  //     setTooltips({
+  //       show: 100,
+  //       hide: 100,
+  //     });
+  //   });
+  // });
+  // function setTooltips(opt) {
+  //   $btns.each(function () {
+  //     $(this).data('bs.tooltip').options.delay = opt;
+  //     console.log($btns.data('bs.tooltip').options.delay);
+  //   });
+  // }
 });
